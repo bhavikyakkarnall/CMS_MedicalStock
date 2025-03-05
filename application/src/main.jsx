487fe1ea19bf95f23684acfb6a4a35c7c46dee0b
bhavikyakkarnall <bhavikyakkarnall@gmail.com>
2025-03-05 22:04:40 +1300

    updated main.jsx

--- application/src/main.jsx
+++ application/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './components/Home.jsx'
+import AppRoute from './routes/AppRoute.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppRoute/>
+  </React.StrictMode>,
 )
